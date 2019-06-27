8bcdd090c4bbcc6a83e9eb9e7ed581ff6fb36d65
Nattanun <chanchaochai@gmail.com>
2019-06-27 15:15:14 -0400

    Add files via upload

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,3 +1,89 @@
 PennController.ResetPrefix(null); // Initiates PennController
 
+PennController.Sequence( "welcome" , "experiment" , "send" , "final" ) //or you can randomize the experiment
+
+PennController( "welcome" ,
+    defaultText
+        .print()
+    ,
+    newText("<p>Welcome!</p>")
+    ,
+    newText("<p>In this experiment, you will have to report which of two pictures matches a description.</p>")
+    ,
+    newText("<p>Press the <strong>F</strong> key for the picture on the left, or the <strong>J</strong> key for the picture on the right.</p>")
+    ,
+    newText("<p>Please enter your ID and then click the button below to start the experiment.</p>")
+    ,
+    newTextInput("ID")
+        .print()
+    ,
+    newButton("Start")
+        .print()
+        .wait()
+    ,
+    newVar("ID")
+        .settings.global()
+        .set( getTextInput("ID") )
+)
+.log( "ID" , getVar("ID") )
+
+
 // Start typing your code here
+PennController.Template( 
+  variable => PennController( "experiment" ,
+    newTimer(500)
+        .start()
+        .wait()
+    ,
+    newAudio("description", variable.AudioFile)
+        .play()
+    ,
+    newText(variable.Description)
+        .unfold(2600)
+    ,
+    newImage("visible", variable.ImageFile)
+        .settings.size(400,400)
+    ,
+    newImage("cov", "Covered_Box.jpg")
+        .settings.size(400,400)
+    ,
+    newCanvas(850,400)
+        .settings.add(   0 , 0 , getImage("visible") )
+        .settings.add( 450 , 0 , getImage("cov") )
+        .print()
+    ,
+    newSelector()
+        .settings.add( getImage("visible") , getImage("cov") )
+        .settings.keys(          "F"    ,          "J"   )
+        .settings.log()
+        .wait()
+    ,
+    getAudio("description")
+       .wait("first")
+    ,
+    newTimer(500)
+        .start()
+        .wait()
+  )
+  .log( "ID" , getVar("ID") )
+  .log( "Item"   , variable.Item   )
+  .log( "Group"  , variable.Group  )
+  .log( "MainCond" , variable.MainCond )
+  .log( "Condition" , variable.Condition )
+)
+
+//Also log age and gender
+
+
+// Completion screen
+PennController.SendResults( "send" )
+PennController( "final" ,
+    newText("<p>Thank you for your participation!</p>")
+        .print()
+    ,
+    newText("<p><a href='https://www.put.your/platform/confirmation/link.here'>Click here to validate your participation.</a></p>")
+        .print()
+    ,
+    newButton("void")
+        .wait()
+)
