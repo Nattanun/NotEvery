380d429af18b52e8653c320a49ede74955148b9a
Nattanun <chanchaochai@gmail.com>
2019-06-29 11:55:48 -0400

    Add files via upload

--- data_includes/main.js
+++ data_includes/main.js
@@ -49,6 +49,7 @@ PennController( "welcome" ,
 					.failure( newText("โปรดเลือกเพศกำเนิดของท่านด้วยค่ะ")
 					.print().remove())
 			)
+)
 .log( "age" , getVar("age") )
 .log( "gender" , getVar("gender") )
 
