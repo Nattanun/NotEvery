69803b9151f9dc06950610f43497c3b776e7488c
Nattanun <chanchaochai@gmail.com>
2019-06-29 09:51:37 -0400

    Add files via upload

--- data_includes/main.js
+++ data_includes/main.js
@@ -55,8 +55,35 @@ PennController( "welcome" ,
 
 // Start typing your code here
 
+newAudio("description", "2fishRoundTank.mp3")
+    .play()
+,
+newText("The fish swim in a tank which is perfectly round")
+    .unfold(2600)
+,
+newImage("two", "2fishRoundTank.png")
+    .settings.size(200,200)
+,
+newImage("one", "1fishSquareTank.png")
+    .settings.size(200,200)
+,
+newCanvas(450,200)
+    .settings.add(   0 , 0 , getImage("two") )
+    .settings.add( 250 , 0 , getImage("one") )
+    .print()
+,
+// newKey("FJ")
+newSelector()
+    .settings.add( getImage("two") , getImage("one") )
+    .settings.keys(          "F"    ,          "J"   )
+    .settings.log()
+    .wait()
+,
+getAudio("description")
+   .wait("first")
+   
 PennController( "practiceA" ,
-    newTimer(1000)
+    newTimer(500)
         .start()
         .wait()
     ,
@@ -90,6 +117,13 @@ PennController( "practiceA" ,
         .settings.add( getImage("ImageVisPracticeA") , getImage("Pcov") )
         .settings.keys(          "F"    ,          "J"   )
         .settings.log()
+        .wait()
+	,
+    getAudio("audPracticeA")
+       .wait("first")	
+	,
+	newTimer(500)
+        .start()
         .wait()
   )
   .log( "age" , getVar("age") )
